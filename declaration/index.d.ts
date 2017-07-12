5b70aa2a689c4ad770230a589423f303eb372634
Willian Krueger <willian.krueger@avalara.com>
2017-07-12 17:12:19 -0300

    new ts releases add requirement for more overloads!

--- declaration/index.d.ts
+++ declaration/index.d.ts
@@ -1,16 +1,22 @@
-type SrcFn04<R1,R2,R3,R4> = (cb : (err:Error, r1:R1, r2:R2, r3:R3, r4:R4) => any ) => any
-type SrcFn14<R1,R2,R3,R4,I1> = (i1:I1, cb : (err:Error, r1:R1, r2:R2, r3:R3, r4:R4) => any ) => any
-type SrcFn24<R1,R2,R3,R4,I1,I2> = (i1:I1, i2:I2, cb : (err:Error, r1:R1, r2:R2, r3:R3, r4:R4) => any ) => any
-type SrcFn34<R1,R2,R3,R4,I1,I2,I3> = (i1:I1, i2:I2, i3:I3, cb : (err:Error, r1:R1, r2:R2, r3:R3, r4:R4) => any ) => any
-type SrcFn44<R1,R2,R3,R4,I1,I2,I3, I4> = (i1:I1, i2:I2, i3:I3, i4:I4, cb : (err:Error, r1:R1, r2:R2, r3:R3, r4:R4) => any ) => any
+type SrcFn04<R1, R2, R3, R4> = (cb: (err: Error, r1: R1, r2: R2, r3: R3, r4: R4) => any) => any
+type SrcFn11<R1, I1> = (i1: I1, cb: (err: Error, r1: R1) => any) => any
+type SrcFn12<R1, R2, I1> = (i1: I1, cb: (err: Error, r1: R1, r2: R2) => any) => any
+type SrcFn13<R1, R2, R3, I1> = (i1: I1, cb: (err: Error, r1: R1, r2: R2, r3: R3) => any) => any
+type SrcFn14<R1, R2, R3, R4, I1> = (i1: I1, cb: (err: Error, r1: R1, r2: R2, r3: R3, r4: R4) => any) => any
+type SrcFn24<R1, R2, R3, R4, I1, I2> = (i1: I1, i2: I2, cb: (err: Error, r1: R1, r2: R2, r3: R3, r4: R4) => any) => any
+type SrcFn34<R1, R2, R3, R4, I1, I2, I3> = (i1: I1, i2: I2, i3: I3, cb: (err: Error, r1: R1, r2: R2, r3: R3, r4: R4) => any) => any
+type SrcFn44<R1, R2, R3, R4, I1, I2, I3, I4> = (i1: I1, i2: I2, i3: I3, i4: I4, cb: (err: Error, r1: R1, r2: R2, r3: R3, r4: R4) => any) => any
 
 
 interface PromisifyT {
-    <R1,R2,R3,R4>(fn:SrcFn04<R1,R2,R3,R4>) : Promise<[R1,R2,R3,R4]>
-    <R1,R2,R3,R4,I1>(fn:SrcFn14<R1,R2,R3,R4,I1>, i1:I1) : Promise<[R1,R2,R3,R4]>
-    <R1,R2,R3,R4,I1,I2>(fn:SrcFn24<R1,R2,R3,R4,I1,I2>, i1:I1, i2:I2) : Promise<[R1,R2,R3,R4]>
-    <R1,R2,R3,R4,I1,I2,I3>(fn:SrcFn34<R1,R2,R3,R4,I1,I2,I3>, i1:I1, i2:I2, i3:I3) : Promise<[R1,R2,R3,R4]>
-    <R1,R2,R3,R4,I1,I2,I3,I4>(fn:SrcFn44<R1,R2,R3,R4,I1,I2,I3,I4>, i1:I1, i2:I2, i3:I3, i4:I4) : Promise<[R1,R2,R3,R4]>
+    <R1, R2, R3, R4>(fn: SrcFn04<R1, R2, R3, R4>): Promise<[R1, R2, R3, R4]>
+    <R1, I1>(fn: SrcFn11<R1, I1>, i1: I1): Promise<[R1]>
+    <R1, R2, I1>(fn: SrcFn12<R1, R2, I1>, i1: I1): Promise<[R1, R2]>
+    <R1, R2, R3, I1>(fn: SrcFn13<R1, R2, R3, I1>, i1: I1): Promise<[R1, R2, R3]>
+    <R1, R2, R3, R4, I1>(fn: SrcFn14<R1, R2, R3, R4, I1>, i1: I1): Promise<[R1, R2, R3, R4]>
+    <R1, R2, R3, R4, I1, I2>(fn: SrcFn24<R1, R2, R3, R4, I1, I2>, i1: I1, i2: I2): Promise<[R1, R2, R3, R4]>
+    <R1, R2, R3, R4, I1, I2, I3>(fn: SrcFn34<R1, R2, R3, R4, I1, I2, I3>, i1: I1, i2: I2, i3: I3): Promise<[R1, R2, R3, R4]>
+    <R1, R2, R3, R4, I1, I2, I3, I4>(fn: SrcFn44<R1, R2, R3, R4, I1, I2, I3, I4>, i1: I1, i2: I2, i3: I3, i4: I4): Promise<[R1, R2, R3, R4]>
 }
 
 /**
